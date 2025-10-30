3bd4877a1494e43de301f65e1674f56609f99c5d
Alexis4am <alexismendezp1@outlook.com>
2025-10-30 01:02:14 -0500

    I overloaded (If that's what this is called) the + and the - operators so I can add and substract vectors, I also added a function that uses this overloaded operators to add a vector multiplied by a scalar. Again, this all came from a book, but I am trying lol

--- core.h
+++ core.h
@@ -36,15 +36,41 @@ namespace cyclone {
             (*this) *= ((real)1)/l;
             }
         }
+        void addScaledVector(const Vector3& v, real scale){
+            x += v.x * scale ;
+            y += v.y * scale ;
+            z += v.z * scale ;
+
+        }
        void operator *= (const real value){
         x *= value;
         y *= value;
         z *= value;
         }
+        void operator += (const Vector3& v){
+            x += v.x;
+            y += v.y;
+            z += v.z;
+        }
+        void operator -= (const Vector3& v){
+            x -= v.x;
+            y -= v.y;
+            z -= v.z;
+        }
 
         Vector3 operator*(const real value) const{
         return Vector3(x*value, y*value, z*value);
         }
+
+        Vector3 operator+(const Vector3& v) const{
+        return Vector3(x+v.x, y+v.y, z+v.z);
+        }
+        Vector3 operator-(const Vector3& v) const{
+        return Vector3(x-v.x, y-v.y, z-v.z);
+        }
+
+
+
 
     };
 }
