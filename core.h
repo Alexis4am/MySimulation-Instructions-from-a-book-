a310cecd489e709f1eb61d624bd4d823891584c0
Alexis4am <kevinmendezp01@gmail.com>
2025-10-31 23:11:08 -0500

    Added hadamard product/dot product. Also formatted the code. :3

--- core.h
+++ core.h
@@ -6,75 +6,146 @@ namespace cyclone {
 
     class Vector3 {
     public:
+        // Values inside of each vector (A 3d Vector duh)  
         real x;
         real y;
         real z;
 
     private:
+        // Used for memory optimization  
         real pad;
     
     public:
+
+        // Constructor (Empty)
         Vector3() : x(0), y(0), z(0) {}
+
+        // Constructor (Assigned values)
         Vector3(real x, real y, real z) : x(x), y(y), z(z) {}
 
-        
-        void invert() {
+
+        // Inverts the Vector 
+        void invert() 
+        {
             x = -x; 
             y = -y; 
             z = -z;
         }
-        real magnitude() const{
+
+        //---------------------------------------------------------------
+
+        // Magnitude of the vector
+        real magnitude() const
+        {
             return real_sqrt(x*x + y*y + z*z);
         }
 
-        real squareMagnitude() const{
+        // Square magnitude of the vector (Since it doesn't take the Sqrt)
+        real squareMagnitude() const
+        {
             return x*x + y*y + z*z;
         }
-        void normalize(){
+
+        // Calculates the unit vector
+        void normalize()
+        {
             real l = magnitude();
             if (l > 0){
             (*this) *= ((real)1)/l;
             }
         }
-        void addScaledVector(const Vector3& v, real scale){
+
+        // Adds a vector that was previously multiplied by a scalar
+        void addScaledVector(const Vector3& v, real scale)
+        {
             x += v.x * scale ;
             y += v.y * scale ;
             z += v.z * scale ;
 
         }
-        //All of this does change the value of the vector
-        //Changes the values
-       void operator *= (const real value){
+
+        //---------------------------------------------------------------
+        //HADAMARD PRODUCT/COMPONENT-WISE PRODUCT 
+
+        // Changes vector:
+        void componentProductUpdate(const Vector3 &vector) //why does the book change the style of referencing? IDK
+        {
+            x *= vector.x; 
+            y *= vector.y;
+            z *= vector.z;
+        }
+
+        // Returns new vector:
+        Vector3 componentProduct(const Vector3 &vector) const 
+        {
+            return Vector3(x * vector.x, y * vector.y, z * vector.z);
+        }
+
+        //---------------------------------------------------------------
+        // SCALES THE VECTOR BY A VALUE
+
+        //Changes vector:
+        void operator *= (const real value)
+        {
         x *= value;
         y *= value;
         z *= value;
         }
-        void operator += (const Vector3& v){
+
+        // Returns new vector:
+        Vector3 operator*(const real value) const
+        {
+        return Vector3(x*value, y*value, z*value);
+        }
+
+        //---------------------------------------------------------------
+        //ADDS THE VECTOR WITH ANOTHER
+
+        // Changes vector:
+        void operator += (const Vector3& v)
+        {
             x += v.x;
             y += v.y;
             z += v.z;
         }
-        void operator -= (const Vector3& v){
+
+        // Returns new vector:
+        Vector3 operator+(const Vector3& v) const
+        {
+        return Vector3(x+v.x, y+v.y, z+v.z);
+        }
+
+        //---------------------------------------------------------------
+        //SUBSTRACT THE VECTOR BY A VALUE:
+
+        // Changes vector:
+        void operator -= (const Vector3& v)
+        {
             x -= v.x;
             y -= v.y;
             z -= v.z;
         }
 
-        //This doesn't change the values of the vector
-        //Returns a new vector
-        Vector3 operator*(const real value) const{
-        return Vector3(x*value, y*value, z*value);
-        }
-
-        Vector3 operator+(const Vector3& v) const{
-        return Vector3(x+v.x, y+v.y, z+v.z);
-        }
-        Vector3 operator-(const Vector3& v) const{
+        // Returns new vector:
+        Vector3 operator-(const Vector3& v) const
+        {
         return Vector3(x-v.x, y-v.y, z-v.z);
         }
 
+        //---------------------------------------------------------------
+        //DOT PRODUCT/SCALAR PRODUCT (Returns a Scalar not a VECTOR):
 
+        real scalarProduct(const Vector3& vector) const
+        {
+            return (x * vector.x, y * vector.y, z * vector.z);
+        }
 
+        real operator *(const Vector3 &vector) const
+        {
+        return x*vector.x + y*vector.y + z*vector.z;
+        }
+        
+        //---------------------------------------------------------------
 
     };
 }
